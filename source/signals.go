1a4d4bce3423c52c1978f56e88c0567cc136719e
gogozhao <zhaowanghang@foxmail.com>
2015-05-14 16:12:35 +0800

    add signal

---
+++ source/signals.go
@@ -0,0 +1,25 @@
+package source
+import (
+	"os"
+	"os/signal"
+	"syscall"
+	"fmt"
+)
+
+func Signals() {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		sig := <-sigs
+		fmt.Println("")
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
